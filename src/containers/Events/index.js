299eda8b111721bf5b1fd4e989c28ea4773b3045
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-07-11 16:59:29 +0200

    correction du probleme de catégorie, plus aucun probleme

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,26 +11,31 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  console.log(data);
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((type ? data?.events : data?.events) || []).filter(
-    (event, index) => {
-      if (
-        (currentPage - 1) * PER_PAGE <= index &&
-        PER_PAGE * currentPage > index
-      ) {
-        return true;
-      }
-      return false;
+
+  const filteredEvents = (
+    (!type
+      ? data?.events
+      : data?.events.filter((event) => event.type === type)) || []
+  ).filter((event, index) => {
+    if (
+      (currentPage - 1) * PER_PAGE <= index &&
+      PER_PAGE * currentPage > index
+    ) {
+      return true;
     }
-  );
+
+    return false;
+  });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((events) => events.type));
+  const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
       {error && <div>An error occured</div>}
